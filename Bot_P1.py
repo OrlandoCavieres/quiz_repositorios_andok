6a00db0650c7c19eea4ee899a27cdb3a222f93ed
Orlando Cavieres <frizortake@gmail.com>
2021-11-21 20:43:50 -0300

    Main function in Bot_P1 added

--- Bot_P1.py
+++ Bot_P1.py
@@ -14,3 +14,13 @@ def bop(update, context):
     context.bot.send_photo(chat_id = chat_id, photo = get_url())
 
 
+def main():
+    updater = Updater('2145587260:AAHERLnOnFYRrM2ymJT0vH4PL28yDaUQKqk', use_context = True)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler('bop', bop))
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
