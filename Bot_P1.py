720227f3fdc029f3d06b24a8aeb420e50390c79f
Orlando Cavieres <frizortake@gmail.com>
2021-11-21 20:40:55 -0300

    Bop function added

--- Bot_P1.py
+++ Bot_P1.py
@@ -7,3 +7,10 @@ import re
 def get_url():
     return requests.get('https://random.dog/woof.json').json()['url']
 
+
+@run_async
+def bop(update, context):
+    chat_id = update.message.chat_id
+    context.bot.send_photo(chat_id = chat_id, photo = get_url())
+
+
